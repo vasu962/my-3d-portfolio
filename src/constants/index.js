db5cf4b043e5cb2028dea8d1b4c2815b3000da3e
Vasu <vasukumar962@gmail.com>
2024-04-01 11:54:06 +0530

    fixed typo

--- src/constants/index.js
+++ src/constants/index.js
@@ -31,26 +31,26 @@ import {
   todo,
   music,
   threejs,
-} from "../assets";
+} from '../assets';
 
 export const navLinks = [
   {
-    id: "about",
-    title: "About",
+    id: 'about',
+    title: 'About',
   },
   {
-    id: "work",
-    title: "Work",
+    id: 'work',
+    title: 'Work',
   },
   {
-    id: "contact",
-    title: "Contact",
+    id: 'contact',
+    title: 'Contact',
   },
 ];
 
 const services = [
   {
-    title: "Web Developer",
+    title: 'Web Developer',
     icon: web,
   },
   // {
@@ -62,42 +62,42 @@ const services = [
   //   icon: backend,
   // },
   {
-    title: "Content Creator",
+    title: 'Content Creator',
     icon: creator,
   },
 ];
 
 const technologies = [
   {
-    name: "HTML 5",
+    name: 'HTML 5',
     icon: html,
   },
   {
-    name: "CSS 3",
+    name: 'CSS 3',
     icon: css,
   },
   {
-    name: "JavaScript",
+    name: 'JavaScript',
     icon: javascript,
   },
   {
-    name: "TypeScript",
+    name: 'TypeScript',
     icon: typescript,
   },
   {
-    name: "React JS",
+    name: 'React JS',
     icon: reactjs,
   },
   {
-    name: "Redux Toolkit",
+    name: 'Redux Toolkit',
     icon: redux,
   },
   {
-    name: "Tailwind CSS",
+    name: 'Tailwind CSS',
     icon: tailwind,
   },
   {
-    name: "Next JS",
+    name: 'Next JS',
     icon: nextjs,
   },
   // {
@@ -109,19 +109,19 @@ const technologies = [
   //   icon: mongodb,
   // },
   {
-    name: "Three JS",
+    name: 'Three JS',
     icon: threejs,
   },
   {
-    name: "git",
+    name: 'git',
     icon: git,
   },
   {
-    name: "Router",
+    name: 'Router',
     icon: router,
   },
   {
-    name: "Postman",
+    name: 'Postman',
     icon: postman,
   },
   // {
@@ -129,23 +129,23 @@ const technologies = [
   //   icon: figma,
   // },
   {
-    name: "docker",
+    name: 'docker',
     icon: docker,
   },
 ];
 
 const experiences = [
   {
-    title: "Frontend developer",
-    company_name: "Google Developer Student Clubs GEU",
+    title: 'Frontend developer',
+    company_name: 'Google Developer Student Clubs GEU',
     icon: era,
-    iconBg: "#383E56",
-    date: "August 2021 - July 2022",
+    iconBg: '#383E56',
+    date: 'August 2021 - September 2022',
     points: [
-      "Was responsible for overseeing the club’s intersections with the public by implementing social media platform content strategies.",
-      "Communicated with back-end developers for any front-end problematic issues.",
-      // "Implementing responsive design and ensuring cross-browser compatibility.",
-      // "Participating in code reviews and providing constructive feedback to other developers.",
+      'Developed a full-stack application using React, Express, MongoDB, and Docker to analyze GitHub data.',
+      'Opened 26 issues, including bug reports, feature requests, and documentation improvements, and successfully closed 20 issues.',
+      'Was responsible for overseeing the club’s intersections with the public by implementing social media platform content strategies.',
+      'Communicated with back-end developers for any front-end problematic issues.',
     ],
   },
   // {
@@ -192,105 +192,105 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
+    name: 'Sara Lee',
+    designation: 'CFO',
+    company: 'Acme Co',
+    image: 'https://randomuser.me/api/portraits/women/4.jpg',
   },
   {
     testimonial:
       "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
+    name: 'Chris Brown',
+    designation: 'COO',
+    company: 'DEF Corp',
+    image: 'https://randomuser.me/api/portraits/men/5.jpg',
   },
   {
     testimonial:
       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+    name: 'Lisa Wang',
+    designation: 'CTO',
+    company: '456 Enterprises',
+    image: 'https://randomuser.me/api/portraits/women/6.jpg',
   },
 ];
 
 const projects = [
   {
-    name: "Ecommerce",
+    name: 'Ecommerce',
     description:
-      "A fully functional e-commerce application with complete stripe payment integration and with sanity platform for structured data.",
+      'A fully functional e-commerce application with complete stripe payment integration and with sanity platform for structured data.',
     tags: [
       {
-        name: "nextjs",
-        color: "blue-text-gradient",
+        name: 'nextjs',
+        color: 'blue-text-gradient',
       },
       {
-        name: "typescript",
-        color: "green-text-gradient",
+        name: 'typescript',
+        color: 'green-text-gradient',
       },
       {
-        name: "stripe",
-        color: "pink-text-gradient",
+        name: 'stripe',
+        color: 'pink-text-gradient',
       },
       {
-        name: "sanity",
-        color: "blue-text-gradient",
+        name: 'sanity',
+        color: 'blue-text-gradient',
       },
       {
-        name: "react-context",
-        color: "green-text-gradient",
+        name: 'react-context',
+        color: 'green-text-gradient',
       },
       {
-        name: "react-hot-toast",
-        color: "pink-text-gradient",
+        name: 'react-hot-toast',
+        color: 'pink-text-gradient',
       },
     ],
     image: headphone,
-    source_code_link: "https://github.com/vasu962/ecommerce",
+    source_code_link: 'https://github.com/vasu962/ecommerce',
   },
   {
-    name: "Music Application",
+    name: 'Music Application',
     description:
-      "A fully fledge spotify like music player that allows to go to the previous song, next song, repeat, shuffle, and fully functional search.",
+      'A fully fledge spotify like music player that allows to go to the previous song, next song, repeat, shuffle, and fully functional search.',
     tags: [
       {
-        name: "react",
-        color: "blue-text-gradient",
+        name: 'react',
+        color: 'blue-text-gradient',
       },
       {
-        name: "tailwind",
-        color: "green-text-gradient",
+        name: 'tailwind',
+        color: 'green-text-gradient',
       },
       {
-        name: "react-router",
-        color: "pink-text-gradient",
+        name: 'react-router',
+        color: 'pink-text-gradient',
       },
       {
-        name: "rapidapi",
-        color: "blue-text-gradient",
+        name: 'rapidapi',
+        color: 'blue-text-gradient',
       },
       {
-        name: "react-hooks",
-        color: "green-text-gradient",
+        name: 'react-hooks',
+        color: 'green-text-gradient',
       },
     ],
     image: music,
-    source_code_link: "https://github.com/vasu962/music-player",
+    source_code_link: 'https://github.com/vasu962/music-player',
   },
   {
-    name: "Keeper Application",
+    name: 'Keeper Application',
     description:
-      "Todo list application just like google keep you keep your notes safe here.",
+      'Todo list application just like google keep you keep your notes safe here.',
     tags: [
       {
-        name: "react",
-        color: "blue-text-gradient",
+        name: 'react',
+        color: 'blue-text-gradient',
       },
       {
-        name: "material-ui",
-        color: "green-text-gradient",
+        name: 'material-ui',
+        color: 'green-text-gradient',
       },
       // {
       //   name: "css",
@@ -298,7 +298,7 @@ const projects = [
       // },
     ],
     image: todo,
-    source_code_link: "https://github.com/vasu962/keeper-app",
+    source_code_link: 'https://github.com/vasu962/keeper-app',
   },
 ];
 
